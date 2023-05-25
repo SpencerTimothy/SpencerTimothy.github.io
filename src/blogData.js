a5f6498534eb113c2be06bf79e36cacb4b8a7c79
SpencerTimothy <spencen000@gmail.com>
2023-05-25 22:42:32 +0200

    added ../blogData.js

---
+++ src/blogData.js
@@ -0,0 +1,49 @@
+const data = [
+  {
+    id: 1,
+    link: "how-i-built-this",
+    img: "../photos/blog_photos/how-i-built-this/how-i-built-this.png",
+    alt: "Website code screenshot",
+    title: "How I Built This Website",
+    description:
+      "The blog post describes the process of building a portfolio website using React, discussing the tools and technologies used, project setup, and website features.",
+  },
+  {
+    id: 2,
+    link: "What's-Next-for-Me",
+    img: "../photos/blog_photos/whats-next/What's Next.png",
+    alt: "alt img 2",
+    title: "What's Next For Me?",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dicta explicabo natus molestiae assumenda aspernatur expedita, voluptatem quam. Eligendi, autem voluptas!",
+  },
+  // {
+  //   id: 3,
+  //   link: "how-i-built-this",
+  //   img: "https://media.istockphoto.com/photos/cloud-computing-picture-id1318623693?s=612x612",
+  //   alt: "alt img 3",
+  //   title: "test 3",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi eos asperiores maiores, veritatis commodi tempora. Obcaecati voluptas fugiat deleniti quam.",
+  // },
+  // {
+  //   id: 4,
+  //   link: "how-i-built-this",
+  //   img: "https://media.istockphoto.com/photos/cloud-computing-picture-id1318623693?s=612x612",
+  //   alt: "alt img 4",
+  //   title: "test 4",
+  //   description:
+  //     "Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid, laudantium expedita. Dicta, aliquam? Dolore et consequatur suscipit excepturi recusandae nam.",
+  // },
+  // {
+  //   id: 5,
+  //   link: "how-i-built-this",
+  //   img: "https://media.istockphoto.com/photos/businessman-with-financial-symbols-picture-id480921984?s=612x612",
+  //   alt: "alt img 5",
+  //   title: "test 5",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque molestias quidem neque enim quo commodi voluptas culpa vitae nihil asperiores!",
+  // },
+];
+
+export default data;
