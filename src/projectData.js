dff0af68cb6b22be0881a4bbbe82cf5758a7c5d6
SpencerTimothy <spencen000@gmail.com>
2023-05-25 22:40:54 +0200

    added ../projectData.js

---
+++ src/projectData.js
@@ -0,0 +1,49 @@
+const data = [
+  {
+    id: 1,
+    link: "Modern-Portfolio-Theory",
+    img: "../photos/project_photos/modernPortfolioTheory/Markowitz Bullet.png",
+    alt: "Markowitz Bullet",
+    title: "Modern Portfolio Theory",
+    description:
+      "A Python project aimed at analyzing and optimizing a diversified portfolio comprising of five ETFs, utilizing the principles of Modern Portfolio Theory.",
+  },
+  {
+    id: 2,
+    link: "US-Mass-Shootings",
+    img: "../photos/project_photos/USMassShootings/U.S. Mass Shootings Map.png",
+    alt: "Geospacial Map of Mass Shootings in the U.S.",
+    title: "U.S. Mass Shootings",
+    description:
+      "A Python-based project to conduct a visual analysis of trends and statistics related to mass shootings in the United States, and featuring an interactive 3D map.",
+  },
+  {
+    id: 3,
+    link: "US-Census-Data",
+    img: "../photos/project_photos/USCensusData/U.S. Census Data.png",
+    alt: "U.S Census Data Tableau Dashboard",
+    title: "U.S. Census Data",
+    description:
+      "An SQL and Tableau project that analyzes U.S. Census data from 2015 to 2017, examining changes in median household income and population for all 52 states and territories.",
+  },
+  {
+    id: 4,
+    link: "Bakery-Sales",
+    img: "../photos/project_photos/bakerySales/Bakery Sales.png",
+    alt: "Tableau dashboard of bakery's sales analysis",
+    title: "Sales Data Analysis",
+    description:
+      "An SQL analysis of a bakery's sales database to analyze customer behavior and preferences, identifying popular foods, flavors, sales figures, and top-spending customers. Visualizations created with Tableau.",
+  },
+  // {
+  //   id: 5,
+  //   link: "modern-portfolio-theory",
+  //   img: "https://media.istockphoto.com/photos/businessman-with-financial-symbols-picture-id480921984?s=612x612",
+  //   alt: "alt img 5",
+  //   title: "test 5",
+  //   description:
+  //     "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque molestias quidem neque enim quo commodi voluptas culpa vitae nihil asperiores!",
+  // },
+];
+
+export default data;
