67664da0662cd3f60a638905370a753c1db9887b
SpencerTimothy <spencen000@gmail.com>
2023-05-25 22:33:24 +0200

    added ../components/ProjectsResumeButtons.js

---
+++ src/components/ProjectsResumeButtons.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import '../CSS/ProjectsResumeButtons.css'
+import { useNavigate } from "react-router-dom";
+
+
+export const ProjectsResumeButtons = () => {
+
+    const navigate = useNavigate();
+
+    return (
+        <div className="button-div">
+            <div className="projects-btn-animate">
+                <button
+                    onClick={() => navigate("/projects")}
+                    className="projects-btn"
+                >
+                See my projects
+                </button>
+            </div>
+            <div className="resume-btn-animate">
+                <button onClick={() => navigate("/resume")} 
+                    className="resume-btn">
+                Jump to resume
+                </button>
+            </div>
+        </div>
+    );
+}
